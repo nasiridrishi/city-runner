25f7f456654340aea7d95aee626bae16a04b5ae7
Mingze <mingez18.2002@gmail.com>
2025-01-23 23:18:01 +0800

    PlayerMovement+CameraSettings

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerMovement : MonoBehaviour
+{
+
+    public Animator animator;
+    private float currentSpeed;
+
+    public CharacterController controller;
+    public float movementSpeed;
+    public float turnTime;
+    float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * movementSpeed * Time.deltaTime);
+        }
+
+        currentSpeed = new Vector3(horizontal, 0, vertical).magnitude * movementSpeed;
+        Debug.Log($"Current Speed: {currentSpeed}");
+        animator.SetFloat("MovementSpeed", currentSpeed);
+    }
+}
