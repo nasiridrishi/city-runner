56bd1d2c6e9ba4ee9deff6df7b5c61fd947ee0dd
Ooi Yan Hong <demoking905@gmail.com>
2025-03-09 17:03:51 +0800

    help menu
    implement help menu, when press tab will show the help menu 5 sec

---
+++ Assets/Scripts/HelpMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class HelpMenu : MonoBehaviour
+{
+    public GameObject helpMenu; // Drag your help menu here
+    private float displayTime = 5f; // Display time
+
+    private bool isShowing = false; // Used to track whether the menu is being displayed
+
+    void Start()
+    {
+        // Show help menu at first and set timer
+        ShowHelpMenu();
+    }
+
+    void Update()
+    {
+        // Detecting Tab key presses
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ShowHelpMenu();
+        }
+    }
+
+    void ShowHelpMenu()
+    {
+        // Show Help Menu
+        helpMenu.SetActive(true);
+        isShowing = true;
+
+        // Set a timer to hide after 5 seconds
+        Invoke("HideHelpMenu", displayTime);
+    }
+
+    void HideHelpMenu()
+    {
+        // Hide Help Menu
+        helpMenu.SetActive(false);
+        isShowing = false;
+    }
+}
