95f2864d740c568da1a209c7f3664f4fe8bcdd64
Dylan Loke <dylanloke4896@gmail.com>
2025-03-09 19:45:08 +0800

    add jump script (problem)

---
+++ Assets/Scripts/player/PlayerJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerJump : MonoBehaviour
+    {
+        //declaring the audio source
+        public AudioSource audioJumpPlayer;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            //get the components from the audio source from the player
+            audioJumpPlayer = GetComponent<AudioSource>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+
+        public void jumpSound()
+        {
+            //play the footstep sounds
+            audioJumpPlayer.Play();
+        }
+    }
+}
